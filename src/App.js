7b9ed336d5d617b3c3ee6041ef0369f63e88b5f6
Marine <marinekislova@gmail.com>
2025-01-09 15:26:56 +0200

    2025/01/09

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@
 import HeroSection from "./components/hero-section/hero-section";
 import { Header } from "./components/hero-section/hero-section";
 
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
     <div className="App">
+     
       <div className="wrapper">
         <div className="content">
-          <Header />
-          <HeroSection />
+        <Header />
+        <HeroSection />
         </div>
       </div>
     </div>
--- src/components/hero-section/hero-section.js
+++ src/components/hero-section/hero-section.js
@@ -11,10 +11,10 @@ const Header = () => {
               <a href="#1">Home</a>
             </li>
             <li>
-              <a href="#1">About</a>
+              <a href="#1">Listing</a>
             </li>
             <li>
-              <a href="#1">Services</a>
+              <a href="#1">Pages</a>
             </li>
             <li>
               <a href="#1">Contact</a>
@@ -28,17 +28,22 @@ const Header = () => {
 
 const HeroSection = () => {
   return (
-    <section className="hero-section">
-      <div className="hero-section__content">
-        <h1>Welcome to Our Website</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel
-          tincidunt erat, vel aliquet nunc. Donec vel neque ac neque fermentum
-          ultricies.
-        </p>
-        <button>Learn More</button>
+    <div className="hero-section">
+      <div className="hero-section__container">
+        <div className="hero-section__row-one">
+          <h1 className="hero-section__content">Explore Our Verified Listings</h1>
+          <p hero-section__content>There are plenty of location-specific and industry-specific business listing sites where you can submit your data.</p>
+          <h4 className="hero-section__rating">Ratings <span>5.0</span></h4>
+          <p className="hero-section__rating">Trusted by Clients From Worldwide</p>
+          <a className="hero-section__contact" href="tel:+6530025656376">+65-3002 56 56 376</a>
+          <p className="hero-section__contact">If you're in a hurry, quick call for us. We are here 24/7 to help you asap.</p>
+        </div>
+        <div className="hero-section__row-two">
+          {/* <img src="image/hero.svg" alt="hero" /> */}
+        </div>
+        <div className="hero-section__row-three"></div>
       </div>
-    </section>
+    </div>
   );
 };
 
